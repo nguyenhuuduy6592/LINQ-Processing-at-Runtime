c5486ac49f2a21c28da549ec6f32cfb9cce5a960
duy.nguyen-huu <duy.nguyen-huu@wize-solutions.com>
2020-03-28 21:32:35 +0700

    Add chart feature

--- AngularJS/Scripts/site.js
+++ AngularJS/Scripts/site.js
@@ -1,10 +1,28 @@
-﻿window.generateDynamicTable = function (dataModel) {
-
-    var dataModelCount = dataModel.length;
-
+﻿window.getFieldList = function () {
+    var dataModelCount = window.dataModel.length;
+    var col = []; // define an empty array
     if (dataModelCount > 0) {
+        // retrieve column header
+        for (var i = 0; i < dataModelCount; i++) {
+            for (var key in window.dataModel[i]) {
+                if (col.indexOf(key) === -1) {
+                    col.push(key);
+                    $('#Fields').append(`<option value="${key}"> 
+                            ${key} 
+                         </option>`);
+                    $('#Values').append(`<option value="${key}"> 
+                            ${key} 
+                         </option>`); 
+                }
+            }
+        }
+    }
+    window.fieldList = col;
+};
 
-
+window.generateDynamicTable = function () {
+    var dataModelCount = window.dataModel.length;
+    if (dataModelCount > 0) {
         // CREATE DYNAMIC TABLE.
         var table = document.createElement("table");
         table.style.width = '50%';
@@ -12,20 +30,11 @@
         table.setAttribute('cellspacing', '0');
         table.setAttribute('cellpadding', '5');
 
-        // retrieve column header ('Name', 'Email', and 'Mobile')
-
-        var col = []; // define an empty array
-        for (var i = 0; i < dataModelCount; i++) {
-            for (var key in dataModel[i]) {
-                if (col.indexOf(key) === -1) {
-                    col.push(key);
-                }
-            }
-        }
+        // retrieve column header
+        var col = window.fieldList;
 
         // CREATE TABLE HEAD .
         var tHead = document.createElement("thead");
-
 
         // CREATE ROW FOR TABLE HEAD .
         var hRow = document.createElement("tr");
@@ -50,7 +59,7 @@
 
             for (var j = 0; j < col.length; j++) {
                 var td = document.createElement("td");
-                td.innerHTML = dataModel[i][col[j]];
+                td.innerHTML = window.dataModel[i][col[j]];
                 bRow.appendChild(td);
             }
             tBody.appendChild(bRow)
@@ -58,11 +67,73 @@
         }
         table.appendChild(tBody);
 
-
         // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
         var divContainer = document.getElementById("dataModel");
         divContainer.innerHTML = "";
         divContainer.appendChild(table);
-
     }
-}
+};
+
+window.generateChart = function () {
+    var dataModelCount = window.dataModel.length;
+    if (dataModelCount > 0) {
+        var ctx = document.getElementById('myChart').getContext('2d');
+        var field = $('#Fields').val();
+        var value = $('#Values').val();
+        var fieldList = $.map(window.dataModel, function (val, index) {
+            return val[field];
+        });
+        var valueList = $.map(window.dataModel, function (val, index) {
+            return val[value];
+        });
+
+        new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: fieldList,
+                datasets: [{
+                    label: '# of Votes',
+                    data: valueList,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        });
+    }
+};
+
+$("#viewGrid").on("click", function () {
+    $("#dataModel").show();
+    $("#myChart").hide();
+    generateDynamicTable();
+});
+
+$("#viewChart").on("click", function () {
+    $("#dataModel").hide();
+    $("#myChart").show();
+    generateChart();
+});
